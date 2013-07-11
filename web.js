cabb5930fa73a303aec8ae787db3c53ae1d89298
Ubuntu <ubuntu@ip-172-31-3-71.eu-west-1.compute.internal>
2013-07-11 15:48:40 +0000

    more changes

--- web.js
+++ web.js
@@ -3,8 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('Hello World2! How are you doing?');
 });
+
+
+var buf = new Buffer(fs.readFileSync('index.html', String));
+
+console.log(buf.toString);
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
